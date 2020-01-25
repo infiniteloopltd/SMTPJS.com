530f7a69914994fdf5083edcdc69b1d53a99c02e
Infinite Loop Development Ltd <fiach.reid@gmail.com>
2020-01-25 19:04:23 +0100

    Update smtp.aspx.cs

--- smtp.aspx.cs
+++ smtp.aspx.cs
@@ -5,8 +5,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Net.Mail;
-using System.Data;
-using System.Data.SqlClient;
 using System.Net;
 using System.IO;
 
